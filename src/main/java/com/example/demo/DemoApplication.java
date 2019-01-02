9a215139a9873dbeaa644500c30d900aab67818c
Mul_Sha <advanced.sharma@daimler.com>
2019-01-02 11:02:38 +0530

    dfasf

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,13 +8,6 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		System.out.println(" = sada");
-		System.out.println(" = sada");
-		System.out.println(" = sada");
-		System.out.println(" = sada");
-		System.out.println(" = sada");
-
-
 	}
 
 }
