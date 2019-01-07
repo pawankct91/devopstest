bdc826c56e05193208d7358d4938759363934b89
Mul_Sha <advanced.sharma@daimler.com>
2019-01-07 21:27:26 +0530

    fds

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -2,15 +2,20 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
 @RestController
 public class StudentController {
 
 
-		@GetMapping("/test")
-	public String getInfo() {
-		System.out.println("truefdfdasfdsa");
-		return "sample data";
+	@GetMapping("/test")
+	public Map<Integer,String> getInfo() {
+		System.out.println("sample data");
+
+	    Map<Integer,String> map=new HashMap<Integer,String>();
+		map.put(1,"Admin");
+		map.put(2,"Employe");
+		return map;
 	}
 	
 
